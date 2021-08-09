10e43f5db2e3b96ccdcd54b1cfd051513c1591cf
George Mavrommatis <gmavrommatis@cognity.gr>
2021-08-09 13:45:57 +0300

    Breaking Change: Jokes app impl

---
+++ src/main/java/guru/springframework/spring5jokesappv2/services/JokeService.java
@@ -0,0 +1,11 @@
+package guru.springframework.spring5jokesappv2.services;
+
+/**
+ * JokeService interface created 09-Aug-21
+ *
+ * @author G.MAVROMMATIS
+ */
+public interface JokeService {
+
+    String getJoke();
+}
---
+++ src/main/java/guru/springframework/spring5jokesappv2/services/JokeServiceImpl.java
@@ -0,0 +1,25 @@
+package guru.springframework.spring5jokesappv2.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * JokeServiceImpl class created 09-Aug-21
+ *
+ * @author G.MAVROMMATIS
+ */
+@Service
+public class JokeServiceImpl implements JokeService{
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
